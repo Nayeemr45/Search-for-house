7b3def552451ab894fe67654adb78845ce60f686
Nayeem Ahmed <45434391+Nayeemr45@users.noreply.github.com>
2020-10-09 22:32:45 +0600

    admin page name change

--- admin_page.php
+++ admin_page.php
@@ -11,7 +11,7 @@
 
   <div class="container-fluid">
       <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top main_nav">
-    <a class="navbar-brand" href="#"><span style= "color : #ff4a4a">Owner</span> Page</a>
+    <a class="navbar-brand" href="#"><span style= "color : #ff4a4a">Admin</span> Page</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
